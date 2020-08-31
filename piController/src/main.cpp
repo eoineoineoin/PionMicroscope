dac2bb8499d9e5a2bec1754b108b514c3b835328
Eoin Mcloughlin <eoinmcl@netsoc.tcd.ie>
2020-08-31 23:38:23 +0100

    Remove old testing code

--- piController/src/main.cpp
+++ piController/src/main.cpp
@@ -39,55 +39,6 @@ int main(void)
 
 	ControlServer controlServer;
 
-//#define OUTPUT_LINEARITY_TEST
-#ifdef OUTPUT_LINEARITY_TEST
-	while(1)
-	{
-		printf("Input: ");
-		int v;
-		scanf("%i", &v);
-		uint16_t i16 = (uint16_t)(v);
-		Write_DAC8532(channel_A, i16);
-	}
-#endif
-
-//#define OUTPUT_TEST
-#ifdef OUTPUT_TEST
-	uint16_t vi = 0;
-	while(1)
-	{
-		Write_DAC8532(channel_A, vi);
-		vi += 1;
-	}
-#endif
-//#define OUTPUT_TEST2
-#ifdef OUTPUT_TEST2
-	const float maxV = 3.3f;
-	while(1)
-	{
-		static float v = 0.0f;
-		v += 0.001f;
-		if(v > maxV) v = 0.0f;
-
-		DAC8532_Out_Voltage(channel_A, v);
-	}
-#endif
-
-//#define INPUT_TEST
-#ifdef INPUT_TEST
-
-	while(1)
-	{
-		float input = ADS1256_GetChannalValue(1);
-		static int _rep = 0;
-		if((_rep++ % 50) == 0)
-		{
-			printf("%f\n", input);
-		}
-		usleep(2 * 1e4);
-	}
-#endif
-
 	BeamController beamController;
 	CommandHandler* commandHandler = &beamController; //<TODO.eoin Probably split this out a different way
 	while(1)
